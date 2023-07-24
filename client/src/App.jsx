cd5917b1446be14c12a0ae111e417ae2d1778120
Gayani Weerasinghe <gayaniw@ewisl.net>
2023-07-24 11:57:57 +0530

    added dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,18 @@ import { useState } from 'react'
 import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Dashbord from './components/Dashboard/Dashboard'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <div>
       <BrowserRouter>
         <Routes>
-         <Route path='/' element={}></Route> 
-         <Route path='/create' element={}></Route>
-         <Route path='/update' element={}></Route>
+         <Route path='/' element={<Dashbord/>}></Route> 
+         {/* <Route path='/create' element={}></Route>
+         <Route path='/update' element={}></Route> */}
         </Routes>
       </BrowserRouter>
     </div>
