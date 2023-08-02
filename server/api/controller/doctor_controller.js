3d1a603e2fd2a470ee0cc4f1d0fd8059307c8c49
Samodya94 <samodyan@ewisl.net>
2023-08-02 11:30:50 +0530

    reseting repo

--- server/api/controller/doctor_controller.js
+++
@@ -1,8 +0,0 @@
-const ApiError = require('../../utilities/Errors/errors');
-const {
-    createDoctor,
-    getAlldoctors,
-    deleteDoctorrByID,
-    getDoctorByID
-  } = require('../services/doctor_services');
-
