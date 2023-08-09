23304e524064625ad2376cca1a96fc3bc288a797
Gayani Weerasinghe <gayaniw@ewisl.net>
2023-08-09 15:47:03 +0530

    Added src folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,24 +2,31 @@ import { useState } from 'react'
 import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { AdminDashboard } from './Components/AdminDashboard/AdminDashboard'
+import { NavBar } from './Components/Navbar/NavBar'
+import { SideBar } from './Components/SideBar/SideBar'
+import { ViewDoctors } from './Components/doctors/ViewDoctors'
 
-import Dashboard from './components/DoctorDashboard/DoctorDashboard'
-
-import Dashbord from './components/Dashboard/Dashboard'
-
-import AdminDashbord from './components/AdminDashboard/Dashboard'
 
 function App() {
-  //const [count, setCount] = useState(0)
+
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
 
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-        
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <div className='grid-container'>
+    <BrowserRouter>
+     <NavBar OpenSidebar={OpenSidebar}/>
+     <SideBar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Routes>
+         <Route path='/admin' element={<AdminDashboard/>}></Route>
+         <Route path='/alldoc' element={<ViewDoctors/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  </div>
   )
 }
 
---
+++ client/src/Components/Navbar/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {BsPersonCircle, BsSearch, BsJustify} from 'react-icons/bs'
+
+export const NavBar = ({OpenSidebar}) => {
+  return (
+    <header className='navbar'>
+      <div className="menu-icon">
+         <BsJustify className='icon' onClick={OpenSidebar}/>
+      </div>
+      <div className="navbar-left">
+         <BsSearch className='icon' />
+      </div>
+      <div className="navbar-right">
+         <BsPersonCircle className='icon' />
+      </div>
+    </header>
+    
+  )
+}
+
